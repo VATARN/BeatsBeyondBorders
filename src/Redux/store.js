091aced409d845ec0d294b7c3c9852fc4c24c4df
Arnav Vats <arnav.vats@utexas.edu>
2023-11-18 01:22:27 -0600

    Finalized UI and Setup Backend

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import userReducer from './userSlice';
 import appReducer from "./appSlice";
 
 export default configureStore({
     reducer: {
-        user: userReducer,
         app: appReducer
     },
 })
--- src/Views/view.js
+++ src/Views/view.js
@@ -1,19 +1,29 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Search from '../components/Search';
 import Dropdown from '../components/Dropdown';
 import Table from '../components/Table';
+import { getHome } from '../API';
 
 const View = () => {
 
-    let [selectedUser, setSelectedUser] = useState("");
-    let [selectedGenre, setSelectedGenre] = useState("");
+    let [selectedUser, setSelectedUser] = useState("None");
+    let [selectedGenre, setSelectedGenre] = useState("None");
+    let [searchString, setSearchString] = useState("");
+
+    useEffect(() => {
+        console.log(selectedUser, selectedGenre, searchString);
+    }, [selectedUser, selectedGenre, searchString]);
+
     const userMenu = [
+        { value: 'None', label: 'None' },
         { value: 'Arnav', label: 'Arnav' },
         { value: 'Becky', label: 'Becky' },
         { value: 'Chad', label: 'Chad' },
         { value: 'Nirjari', label: 'Nirjari' },
     ];
+
     const genreMenu = [
+        { value: 'None', label: 'None' },
         { value: 'Pop', label: 'Pop' },
         { value: 'Hip-Hop', label: 'Hip-Hop' },
         { value: 'Country', label: 'Country' },
@@ -26,11 +36,15 @@ const View = () => {
     const handleGenreChange = (e) => {
         setSelectedGenre(e.target.value);
     }
+    const handleSearchOnChange = (e) => {
+        setSearchString(e.target.value);
+    }
 
     const songs = [
         { "name": "Letsgo", artist: "lestgo1", genre: "letsgo3" },
         { "name": "Letsgo", artist: "lestgo1", genre: "letsgo3" },
     ];
+
     return (
         <React.Fragment>
             <div id="view">
@@ -53,7 +67,7 @@ const View = () => {
                         <Dropdown label="Genre" value={selectedGenre} handleChange={handleGenreChange} menuItems={genreMenu} />
                     </div>
                     <div id="Search" style={{ position: "absolute", right: 0 }}>
-                        <Search />
+                        <Search searchString={searchString} onChange={handleSearchOnChange} />
                     </div>
                 </div>
                 <div id="Table">
